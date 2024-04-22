0c626602d4e60b58b3ab3cae6434cc613c51e939
Jefferson Oliveira <jefferson.oliveira203@etec.sp.gov.br>
2024-04-22 20:34:27 -0300

    get e set

---
+++ dia 22/Pessoa.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Pessoa
+    {
+        private string nome;
+        private double salario;
+
+        public Pessoa(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return this.nome.ToUpper(); ;
+            }
+            set
+            {
+                this.nome = value;
+            }
+
+        }
+
+        public double Salario
+        {
+            get
+            {
+                return this.salario;
+            }
+
+            set
+            {
+                if (this.salario > value)
+                {
+                    Console.WriteLine("Salario atual maior que o informado");
+                }
+                else
+                {
+                    this.salario = value;
+                    Console.WriteLine("Salario atualizado");
+                }
+            }
+
+        }
+        public string Email { get; set; }
+    }
+            
+        
+        
+        
+    
+}
